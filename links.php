383e45b3fc7263f88ac7d8652236fe597153fbd0
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2022-03-31 15:28:01 -0400

    adjusted link column sizing

--- links.php
+++ links.php
@@ -27,14 +27,14 @@
                 <div class="row">
                     <div class="col-12 col-lg-9">
                         <div class="row mt-3">
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a href="https://stau.ycdsb.ca/our-school/staff-directory/" class="link text-red">
                                     <div class="col-12 infobox p-4 my-2">
                                         <h2 class="link-text text-center">Staff Directory</h2>
                                     </div>
                                 </a>
                             </div>
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a href="https://www.ycdsb.ca/" class="link text-red">
                                     <div class="col-12 infobox p-4 my-2">
                                         <h2 class="link-text text-center">YCDSB Website</h2>
@@ -43,17 +43,17 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a
                                     href="https://stau.ycdsb.ca/our-school/calendar/"
                                     class="link text-red"
                                 >
                                     <div class="col-12 infobox p-4 my-2">
-                                        <h2 class="link-text text-center">Events/Calendar</h2>
+                                        <h2 class="link-text text-center">Calendar</h2>
                                     </div>
                                 </a>
                             </div>
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a href="https://sites.google.com/ycdsbk12.ca/staulibrary" class="link text-red">
                                     <div class="col-12 infobox p-4 my-2">
                                         <h2 class="link-text text-center">Library Website</h2>
@@ -62,14 +62,14 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a href="https://titantimes.live/" class="link text-red">
                                     <div class="col-12 infobox p-4 my-2">
                                         <h2 class="link-text text-center">Titan Times</h2>
                                     </div>
                                 </a>
                             </div>
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a href="https://sites.google.com/ycdsbk12.ca/stauguidance" class="link text-red">
                                     <div class="col-12 infobox p-4 my-2">
                                         <h2 class="link-text text-center">Guidance Website</h2>
@@ -78,14 +78,14 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a href="https://covid-19.ontario.ca/school-screening/" class="link text-red">
                                     <div class="col-12 infobox p-4 my-2">
                                         <h2 class="link-text text-center">Daily Screening</h2>
                                     </div>
                                 </a>
                             </div>
-                            <div class="col-6 link-container">
+                            <div class="col-12 col-md-6 link-container">
                                 <a
                                     href="https://app.staugustinechs.ca"
                                     class="link text-red"
@@ -97,10 +97,11 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-lg-3 d-flex flex-column my-3 infobox">
-                        <div class="col-12 text-red p-4 my-2">
-                            <h2 class="link-text text-center">Socials</h2>
-                        </div>
+                <div class="col-12 col-lg-3 d-flex flex-column my-3 infobox">
+                    <div class="col-12 text-red p-4 my-2">
+                        <h2 class="link-text text-center">Socials</h2>
+                    </div>
+                    <div class="col-12 pb-4 my-2">
                         <a href="https://twitter.com/STAUpdates" class="link text-red">
                             <div class="d-flex flex-row justify-content-evenly align-items-center">
                                 <i class="fa fa-twitter link-container" style="font-size: 36px;cursor:pointer;"></i>
@@ -113,6 +114,7 @@
                                 <h3>Instagram</h3>
                             </div>
                         </a>
+                    </div>
                 </div>
                 <div class="row flex-grow-1">
                     <div class="col-12 d-flex flex-column fs-3">
