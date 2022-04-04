6c868ec6cc9ac82e30f55af58f0fbdfbdf211fef
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2022-04-04 11:08:41 -0400

    style: format files

--- additionalinfo.php
+++ additionalinfo.php
@@ -1,56 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            crossorigin="anonymous"
-        />
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link href="assets/css/common.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
 
-        <title>Additional Info | St Augustine CHS App</title>
-    </head>
+    <link href="assets/css/common.css" rel="stylesheet" />
 
-    <body>
-        <div id="loading">
-            <div id="loading-circle"></div>
-        </div>
-        <div class="row m-0 vh-100">
-            <?php include "inc/sidebar.php" ?>
-            <div
-                class="
+    <title>Additional Info | St Augustine CHS App</title>
+</head>
+
+<body>
+    <div id="loading">
+        <div id="loading-circle"></div>
+    </div>
+    <div class="row m-0 vh-100">
+        <?php include "inc/sidebar.php" ?>
+        <div class="
                     d-flex
                     flex-column
                     align-items-center
                     justify-content-center
                     col-12 col-lg-10
                     order-last
-                "
-            >
-                <a class="additionalinfo-text underline my-3" href="https://docs.google.com/a/ycdsb.ca/forms/d/1Q1-u5FjfA-x6L5ZMtVIbELO8vZMx-5h5nBdDQoZzi04/viewform">
-                    St. Augustine Staff, click here to add a new announcement
-                </a>
-                <a class="additionalinfo-text underline my-3" href="https://staugustinechs.ca/sta2021/printable">
-                    Click here to view a printable version of the announcements
-                </a>
-                <p class="additionalinfo-text my-3">
-                    Site Maintained by the St. Augustine CHS App Development Team
-                </p>
-                <p class="additionalinfo-text my-3">St. Augustine CHS Technology</p>
-            </div>
+                ">
+            <a class="additionalinfo-text underline my-3"
+                href="https://docs.google.com/a/ycdsb.ca/forms/d/1Q1-u5FjfA-x6L5ZMtVIbELO8vZMx-5h5nBdDQoZzi04/viewform">
+                St. Augustine Staff, click here to add a new announcement
+            </a>
+            <a class="additionalinfo-text underline my-3" href="https://staugustinechs.ca/sta2021/printable">
+                Click here to view a printable version of the announcements
+            </a>
+            <p class="additionalinfo-text my-3">
+                Site Maintained by the St. Augustine CHS App Development Team
+            </p>
+            <p class="additionalinfo-text my-3">St. Augustine CHS Technology</p>
         </div>
+    </div>
 
-       <script src="assets/js/script.js" async defer></script>
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"
-            async defer
-        ></script>
-    </body>
+    <script src="assets/js/script.js" async defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"
+        async defer></script>
+</body>
+
 </html>
