bedf93acdd46122cc5792c2aac5f2f514e4101e5
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2022-04-04 13:56:32 -0400

    refactor: moved head tags to head.php

--- printable.php
+++ printable.php
@@ -6,12 +6,7 @@
 
 <head>
     <?php include "inc/analytics.php" ?>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-
+    <?php include "inc/head.php" ?>
     <title>Printable Announcements | St Augustine CHS App</title>
 </head>
 
