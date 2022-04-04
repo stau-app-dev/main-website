bb855cccb54bfc3aee268307df92591c0fcd3b75
Martin <46298951+martin226@users.noreply.github.com>
2022-04-04 14:05:49 -0400

    feat: add favicon (#3)
    * refactor: moved head tags to head.php
    
    * feat: add favicon

--- additionalinfo.php
+++ additionalinfo.php
@@ -3,14 +3,7 @@
 
 <head>
     <?php include "inc/analytics.php" ?>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-
-    <link href="assets/css/common.css" rel="stylesheet" />
-
+    <?php include "inc/head.php" ?>
     <title>Additional Info | St Augustine CHS App</title>
 </head>
 
