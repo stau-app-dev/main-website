5d65280a3de1c0b8dfbdb6e2887f8fa1d05471f2
Martin <46298951+martin226@users.noreply.github.com>
2022-04-05 13:24:17 -0400

    fix: printable page styles (#4)

--- printable.php
+++ printable.php
@@ -6,7 +6,11 @@
 
 <head>
     <?php include "inc/analytics.php" ?>
-    <?php include "inc/head.php" ?>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <title>Printable Announcements | St Augustine CHS App</title>
 </head>
 
