83267429770107f1a1cacc50cacd3b2ddf9ca82d
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-24 12:58:32 -0400

    fix: disable auto-anchoring announcements in API

--- src/api/announcements.php
+++ src/api/announcements.php
@@ -1,5 +1,5 @@
 <?php 
     include "../inc/announcements.php";
-    $announcements_array = getAnnounceArray();
+    $announcements_array = getAnnounceArray(false);
     $res = array("announcements" => $announcements_array);
     echo json_encode($res);
